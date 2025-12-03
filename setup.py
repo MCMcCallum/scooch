46f2fb4d1aa0bd2574f208f746e5b779057cfde3
matt_mccallum <matt_mccallum@apple.com>
2025-12-03 13:07:05 +0000

    Bumped version, loosened pyyaml dependency

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ the [documentation](http://www.mattcmccallum.com/scooch/docs).
 
 
 REQUIRED_PACKAGES = [
-    'pyyaml==6.0.1',
+    'pyyaml>=6.0.1',
     'sphinx',
     'sphinx_rtd_theme==0.5.1',
     'ruamel.yaml==0.16.12',
@@ -105,7 +105,7 @@ setuptools.setup(
         'make_reqs': MakeReqsCommand
     },
     name='scooch',
-    version='1.1.0',
+    version='1.1.1',
     description='A python module for configuring hierarchical class structures in yaml with defaults',
     long_description=long_description,
     long_description_content_type="text/markdown",
